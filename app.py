ddf64906614eb6c796c7ae920f9e170be301244c
Nastya <kartashovaanastasia17@gmail.com>
2022-11-21 23:14:05 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import requests
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("index_html.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+

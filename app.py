34e8a0b595a1bad5796f5cb241b33eb6edf3f98f
Nastya <kartashovaanastasia17@gmail.com>
2022-11-22 22:41:03 +0300

    feat: new version

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template
 import requests
+
 app = Flask(__name__)
 
-
 @app.route('/')
-@app.route('/home')
-def home():
-    return render_template("index_html.html")
+def apis():
+    params_1 = {
+        'q': 'Minsk',
+        'appid': 'bf9916088ad2517605a3912d42308578'
+    }
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    response_1 = requests.get('https://api.openweathermap.org/data/2.5/weather', params=params_1)
+    print(response_1.status_code)
+    print(response_1.json())
+
+
+    params_2 = {"currencies": "BYN",
+                "source": "ARS",
+                "apikey": "ftxI3cjCodPhbgauZkMey3RQdIs3Fibu"}
+
+    response_2 = requests.get("https://api.apilayer.com/currency_data/live", params=params_2)
+    return response_2.text
+
+if __name__ == 'main':
+    app.run()
+
 
 
